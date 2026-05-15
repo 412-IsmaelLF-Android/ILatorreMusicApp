8d3117c133fd82bd1b4bc4011278971d1ee17461
Ismael Latorre <latorreismael.30@gmail.com>
2026-05-15 03:06:40 -0600

    Segundo Commit dependencias y libs agregadas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.pjasoft.ilatorreappmusic"
+    namespace = "com.pjasoft.ilatorrefappmusic"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -12,12 +12,11 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.pjasoft.ilatorreappmusic"
+        applicationId = "com.pjasoft.ilatorrefappmusic"
         minSdk = 29
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -33,6 +32,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = false
     }
     buildFeatures {
         compose = true
@@ -48,6 +48,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -55,4 +57,10 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("org.jetbrains.compose.material:material-icons-extended:1.7.3")
+    implementation("io.coil-kt.coil3:coil-compose:3.1.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.1.0")
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
